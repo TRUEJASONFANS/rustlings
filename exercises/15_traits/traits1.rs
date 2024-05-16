2a42789e5b76a2e829da1cbbe5f64cb945e72770
zhxie <472408570@qq.com>
2024-05-16 14:14:35 +0800

    errrors6, generic, traits1, iterator1,2

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -14,7 +14,9 @@ trait AppendBar {
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+         self.to_owned() + "Bar"
+    }
 }
 
 fn main() {
